eb0391d113e1e6bae0ba13c3404545cd874dac44
Josh Lee <joshuaree138@gmail.com>
2021-02-11 14:49:57 -0600

    first commit

---
+++ src/Playlist.java
@@ -0,0 +1,24 @@
+
+public class Playlist {
+
+	private Song[] songs;
+	private int numSongs;
+	private int MIN_CAPACITY;
+	
+	public Playlist()
+	{
+		
+	}
+	
+	public Playlist(int capacity)
+	{
+		
+	}
+	
+	public int getCapacity()
+	{
+		
+	}
+	
+	
+}
